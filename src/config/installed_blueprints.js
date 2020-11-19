40f35d9d757af512b09cb45dec3a2f4018952881
Crowdbotics <team@crowdbotics.com>
2020-11-19 04:45:40 +0000

    add package blueprint-react-native-articles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'ArticleList175638', human_name: 'Article List', access_route: 'ArticleList175638'},
 { name: 'ArticleList175621', human_name: 'Article List', access_route: 'ArticleList175621'},
 { name: 'ArticleList175620', human_name: 'Article List', access_route: 'ArticleList175620'},
 { name: 'ArticleList175619', human_name: 'Article List', access_route: 'ArticleList175619'},
